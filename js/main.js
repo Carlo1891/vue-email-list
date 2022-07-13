acac0d9fa4a4bff7774700a45053fffe715dd71e
Carlo1891 <carlo.18@live.it>
2022-07-13 15:03:32 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// Generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
