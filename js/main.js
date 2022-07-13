fa5158311d3cea63e87fbe6a5083800613e61019
Carlo1891 <carlo.18@live.it>
2022-07-13 15:15:55 +0200

    Sviluppo JS con vue, email casuali

--- js/main.js
+++ js/main.js
@@ -2,5 +2,27 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // Generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-// Bonus
+// Bonus:
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+app = new Vue ({
+    el: '#app',
+    data: {
+        emailList: [],
+    },
+    methods: {
+
+        //funzione per inserire la mail nella lista (link boolean)
+        pushRandomEmail: function() {
+            // utilizzo axios ricorda il link pagina markup
+            axios.get ('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then ((response) =>{
+                console.log (response.data.response);
+                this.emailList.push (response.data.response);
+            });
+        }
+    },
+    created() {
+        this.pushRandomEmail ();
+    }
+}) 
